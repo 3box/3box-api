d986e97628ba083f7d5ca0b1d6f8c6c5bda787d0
Zach Ferland <zachferland@gmail.com>
2019-11-14 23:51:44 -0500

    fix: api ipfs repo dont overwrite pinning keys/configs

--- src/s3.js
+++ src/s3.js
@@ -1,15 +1,43 @@
 const S3Store = require('datastore-s3')
+const S3 = require('aws-sdk/clients/s3')
+const IPFSRepo = require('ipfs-repo')
+
+// Redundant with createRepo in datastore-s3, but need to not pass
+// s3 for root, keys, datastore otherwise overwites pinning keys
+
+// A mock lock
+const notALock = {
+  getLockfilePath: () => {},
+  lock: (_) => notALock.getCloser(),
+  getCloser: (_) => ({
+    close: () => {}
+  }),
+  locked: (_) => false
+}
 
 const ipfsRepo = (config) => {
   const { path, bucket, accessKeyId, secretAccessKey } = config
+  const createIfMissing = false
 
-  return S3Store.createRepo({
-    path
-  }, {
-    bucket,
-    accessKeyId,
-    secretAccessKey,
-    createIfMissing: false
+  const storeConfig = {
+    s3: new S3({
+      params: {
+        Bucket: bucket
+      },
+      accessKeyId,
+      secretAccessKey
+    }),
+    createIfMissing
+  }
+
+  return new IPFSRepo(path, {
+    storageBackends: {
+      blocks: S3Store,
+    },
+    storageBackendOptions: {
+      blocks: storeConfig,
+    },
+    lock: notALock
   })
 }
 
