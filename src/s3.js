83e086cb0cd56b43f96598d9e4767195e97ecd5d
Zach Ferland <zachferland@gmail.com>
2019-11-23 21:30:26 -0500

    feat: local mem ipfs repo parts, ignore remote

--- src/s3.js
+++ src/s3.js
@@ -1,6 +1,8 @@
 const S3Store = require('datastore-s3')
 const S3 = require('aws-sdk/clients/s3')
 const IPFSRepo = require('ipfs-repo')
+const LevelStore = require('datastore-level')
+const memLevel = require('level-mem')
 
 // Redundant with createRepo in datastore-s3, but need to not pass
 // s3 for root, keys, datastore otherwise overwites pinning keys
@@ -30,12 +32,23 @@ const ipfsRepo = (config) => {
     createIfMissing
   }
 
+  const memStoreConfig = {
+    db: memLevel,
+    createIfMissing
+  }
+
   return new IPFSRepo(path, {
     storageBackends: {
       blocks: S3Store,
+      datastore: LevelStore,
+      root: LevelStore,
+      keys: LevelStore
     },
     storageBackendOptions: {
       blocks: storeConfig,
+      datastore: memStoreConfig,
+      root: memStoreConfig,
+      keys: memStoreConfig
     },
     lock: notALock
   })
